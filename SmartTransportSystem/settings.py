b7dde766342aac8a0602ef0dc22e8ff5024141bf
kelvin wepo <kelvinwepo7710@gmail.com>
2024-07-01 23:55:27 +0300

    this commit modifies settings.py

--- SmartTransportSystem/settings.py
+++ SmartTransportSystem/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0y(&(+ke+5sop3wr^(sh)cta&mdq#!890yjxb&(rnil4=s2=dg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
